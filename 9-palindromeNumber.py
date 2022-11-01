6ba240b398a32e65511fc286c5242922b6929b27
Donald Winkelman <dmwink.tx@gmail.com>
2022-11-01 17:09:43 -0400

    Finished 9. isPalindrome

--- 9-palindromeNumber.py
+++ 9-palindromeNumber.py
@@ -1,16 +1,18 @@
-def isPalindrome(self, x: int) -> bool:
-    digits = 0
-    testX = x
-    flippedX = 0
-    while testX != 0:
-        digits += 1
-        testX = testX // 10
-    for i in range(digits):
-        digit = x % pow(10, i)
-        flippedX += digit * pow(10, digits - i)
-    if x == flippedX:
-        return True
-    else:
-        return False
-    
-print(isPalindrome(325, 323))
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if(x < 0):
+            return False
+        digits = 0
+        testX = x
+        flippedX = 0
+        while testX != 0:
+            digits += 1
+            testX = testX // 10
+        for i in range(digits):
+            digit = x % pow(10, i + 1)
+            digit = digit // pow(10, i)
+            flippedX += digit * pow(10, digits - i - 1)
+        if x == flippedX:
+            return True
+        else:
+            return False
